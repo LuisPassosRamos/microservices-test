5f4d4a69e9fda5fccba3a2564f0c3bb62aaec2ca
LuisPassosRamos <20222tadssaj0015@ifba.edu.br>
2024-12-14 23:56:41 -0300

    changes

--- servico_api/app.py
+++ servico_api/app.py
@@ -1,18 +1,14 @@
 from flask import Flask, request, jsonify
-import requests
 
 app = Flask(__name__)
 
-# URL do serviço de banco de dados
 DB_URL = "http://localhost:5001/users"
 
-# Endpoint GET /users (Consome o serviço de banco)
 @app.route("/users", methods=["GET"])
 def get_users():
     response = requests.get(DB_URL)
     return response.json(), response.status_code
 
-# Endpoint POST /users (Consome o serviço de banco)
 @app.route("/users", methods=["POST"])
 def add_user():
     user = request.json
--- servico_db/db.py
+++ servico_db/db.py
@@ -4,28 +4,23 @@ import os
 
 app = Flask(__name__)
 
-# Caminho para o arquivo JSON
 FILE_PATH = "users.json"
 
-# Função para ler o JSON
 def read_users():
     if os.path.exists(FILE_PATH):
         with open(FILE_PATH, "r") as file:
             return json.load(file)
     return []
 
-# Função para escrever no JSON
 def write_users(users):
     with open(FILE_PATH, "w") as file:
         json.dump(users, file, indent=4)
 
-# Endpoint GET /users
 @app.route("/users", methods=["GET"])
 def get_users():
     users = read_users()
     return jsonify(users), 200
 
-# Endpoint POST /users
 @app.route("/users", methods=["POST"])
 def add_user():
     user = request.json
